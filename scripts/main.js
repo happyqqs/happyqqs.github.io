526d94d0c25250d9961da920abc00aa72fb9b460
“shenqianqian01” <shenqianqian01@baidu.com>
2019-06-23 21:26:17 +0800

    1

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,32 @@
 let myImage = document.querySelector('img');
 var num = 1
 
+var jsBridge = function (callback) {
+  if (window.WebViewJavascriptBridge) {
+      return callback(WebViewJavascriptBridge);
+  }
+  else {
+      document.addEventListener('WebViewJavascriptBridgeReady', function() {
+          callback(WebViewJavascriptBridge);
+      }, false);
+  }
+  if (window.WVJBCallbacks) {
+      return window.WVJBCallbacks.push(callback);
+  }
+  window.WVJBCallbacks = [callback];
+  var WVJBIframe = document.createElement('iframe');
+  WVJBIframe.style.display = 'none';
+  WVJBIframe.src = 'https://__bridge_loaded__';
+  document.documentElement.appendChild(WVJBIframe);
+  setTimeout(function() {
+      document.documentElement.removeChild(WVJBIframe);
+  }, 0);
+}
+
+jsBridge(function(bridge) {
+  bridge.init();
+  bridge.callHandler('init', {});
+});
 
 // 个性化欢迎信息
 function setHeading(name) {
